96faecb5f569bc1eef94a397a25d237c8749b645
Marco Minerva <marco.minerva@gmail.com>
2023-02-02 15:00:35 +0100

    Libraries update

--- HangfireSample/Program.cs
+++ HangfireSample/Program.cs
@@ -5,7 +5,6 @@ using HangfireSample.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
